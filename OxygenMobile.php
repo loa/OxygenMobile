8ed8af23d87aea8957c7199296f17142ff95c025
Carl Loa Odin <carlodin@gmail.com>
2010-12-11 12:54:51 +0100

    changed in OxygenMobile.php so it includes different js files dependent on FORUM_PAGE

--- OxygenMobile.php
+++ OxygenMobile.php
@@ -1,4 +1,6 @@
+<?php if(file_exists(FORUM_ROOT.'style/'.$forum_user['style'].'/'.FORUM_PAGE.'.js')) { ?>
 <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.4.4/jquery.min.js"></script> 
-<script type="text/javascript" src="<?php echo $base_url.'/style/'.$forum_user['style'] ?>/mobile.js"></script> 
+<script type="text/javascript" src="<?php echo $base_url.'/style/'.$forum_user['style'].'/'.FORUM_PAGE; ?>.js"></script> 
+<?php } ?>
 <link rel="stylesheet" type="text/css" media="screen" href="<?php echo $base_url.'/style/'.$forum_user['style'] ?>/OxygenMobile.css" />
 <link rel="stylesheet" type="text/css" media="screen" href="<?php echo $base_url.'/style/'.$forum_user['style'] ?>/OxygenMobile_cs.css" />
